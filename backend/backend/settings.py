1fa9ccedcbfbc8d8aad88126a6733f082eb4a101
stas <babenk0fff@gmail.com>
2023-04-20 02:46:32 -0700

    Added logging

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -158,3 +158,36 @@ if not DEBUG:
             'rest_framework.renderers.JSONRenderer',
         ]
     }
+
+
+# Logging
+
+LOG_FILE = BASE_DIR / "var" / "log" / "main_log.log"
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_logers': False,
+    'formatters': {
+        'console': {
+            'format': '[%(asctime)s] %(levelname)s %(name)s (%(lineno)d)%(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': LOG_FILE,
+            'formatter': 'console'
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console'
+        },
+    },
+    'loggers': {
+        'django': {
+            'level': 'INFO',
+            'handlers': ['file', 'console']
+        },
+    },
+}
