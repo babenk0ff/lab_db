5030d5298e30535ecfc575b49badd23bcb5bcd35
stas <babenk0fff@gmail.com>
2023-08-01 06:18:07 -0700

    Parsing form refactoring

--- backend/deviceapp/field_validators.py
+++
@@ -1,7 +0,0 @@
-from django.core.validators import RegexValidator
-
-org_code_validator = RegexValidator(
-    regex=r'^[а-яА-Я]{4}$',
-    message='Код должен состоять из четырех кириллических букв',
-    code='invalid_code',
-)
